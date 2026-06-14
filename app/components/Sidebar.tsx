a4863916a051a2bcaed763cf6c08051c840371a7
NTMQuannuaQMTN <nguyentruongmanhquan@gmail.com>
2026-06-14 19:27:19 +0700

    update again

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div className="flex w-full h-full flex-row md:flex-col bg-red-700
+    text-black py-3 px-4 md:justify-start
+    md:rounded-r-2xl rounded-b-2xl">
+      <h1 className="font-bold text-2xl md:text-xl">qnrj</h1>
+    </div>
+  );
+}
